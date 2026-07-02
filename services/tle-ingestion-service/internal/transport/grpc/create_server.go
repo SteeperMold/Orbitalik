b1f74a4b9d865e8a1f14be38187a5cececb6d80b
steepermold <bkmz22092008@gmail.com>
2026-07-02 18:41:43 +0400

    feat(go-common): add logging interceptor

--- services/tle-ingestion-service/internal/transport/grpc/create_server.go
+++ services/tle-ingestion-service/internal/transport/grpc/create_server.go
@@ -1,13 +1,14 @@
 package grpc
 
 import (
+	"github.com/SteeperMold/Orbitalik/common/go/grpc/interceptors"
 	applog "github.com/SteeperMold/Orbitalik/common/go/log"
 	"google.golang.org/grpc"
 )
 
 func NewServer(logger applog.Logger) *grpc.Server {
 	server := grpc.NewServer(
-		grpc.UnaryInterceptor(UnaryLoggingInterceptor(logger)),
+		grpc.UnaryInterceptor(interceptors.UnaryLoggingInterceptor(logger)),
 	)
 
 	return server
